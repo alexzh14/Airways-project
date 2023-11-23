5d52081b33305ebee6bf5807471532e7880dc4cf
Alex Zh <alzh147@gmail.com>
2023-11-23 22:09:43 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15_000;
+        int bonus = price / 20;
+
+        System.out.println("Количество накопленных миль:" + bonus);
+          }
+}
